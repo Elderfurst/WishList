2c4afbefe4ec0d5c4dd86ac9d21aeb97e54a1826
Nicholas Anderson <njcanderson@gmail.com>
2021-01-14 14:19:26 -0800

    Add foreign key to wish list entry model

--- WishList/Data/Models/WishListEntry.cs
+++ WishList/Data/Models/WishListEntry.cs
@@ -5,6 +5,7 @@ namespace WishList.Data.Models
 	public class WishListEntry
 	{
 		public int Id { get; set; }
+		public int WishListRecordId { get; set; }
 		public string Name { get; set; }
 		public string BackingUrl { get; set; }
 		public DateTime DateAdded { get; set; }
